70bf33e527a4e1ef01a4917b8905eab63d08f344
Sarah Wilson <szw0070@auburn.edu>
2021-02-23 13:58:29 -0600

    The first test is working: if nothing is entered as the "sequence", nothing is returned.

--- translate.py
+++ translate.py
@@ -75,8 +75,14 @@ def get_reverse(sequence):
     >>> get_reverse('AUGC')
     'CGUA'
     """
-    sequence = sys.argv[1]
-    print (sequence[::-1])
+    print("\n\n sequence: " + sequence)
+    sequence = sequence.upper()
+    sequence = sequence [::-1]
+    print("reverse sequence: " + sequence)
+    if (sequence != ''):
+        return(sequence)
+    else:
+        return ''
 
 def get_complement(sequence):
     """Get the complement of a `sequence` of nucleotides.
@@ -90,7 +96,14 @@ def get_complement(sequence):
     >>> get_complement('AUGC')
     'UACG'
     """
-    pass
+    print("\n\n sequence: " + sequence)
+    sequence = sequence.upper()
+    print("complement: " + sequence)
+    if (sequence != ''):
+        return(sequence)
+    else:
+        return ''
+
 
 def reverse_and_complement(sequence):
     """Get the reversed and complemented form of a `sequence` of nucleotides.
